e0d1e42f33e0feca380148fc462eacfc0340697c
Josh Long <54473+joshlong@users.noreply.github.com>
2026-08-24 01:55:56 -0700

    consolidation

--- desktop-client/src/main/java/com/example/bootiful_javafx/BootifulJavafxApplication.java
+++ desktop-client/src/main/java/com/example/bootiful_javafx/BootifulJavafxApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilte
 import org.springframework.boot.security.autoconfigure.web.servlet.ServletWebSecurityAutoConfiguration;
 import org.springframework.boot.security.oauth2.client.autoconfigure.servlet.OAuth2ClientWebSecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
@@ -17,8 +16,11 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
+import org.springframework.security.oauth2.client.web.client.support.OAuth2RestClientHttpServiceGroupConfigurer;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.service.registry.ImportHttpServices;
 
+@ImportHttpServices(UserinfoClient.class)
 @SpringBootApplication(exclude = { ServletWebSecurityAutoConfiguration.class, SecurityFilterAutoConfiguration.class,
 		UserDetailsServiceAutoConfiguration.class, OAuth2ClientWebSecurityAutoConfiguration.class })
 public class BootifulJavafxApplication {
@@ -44,6 +46,12 @@ public class BootifulJavafxApplication {
 	@Bean
 	RestClient restClient(RestClient.Builder builder, OAuth2AuthorizedClientManager authorizedClientManager) {
 		return builder.requestInterceptor(new OAuth2ClientHttpRequestInterceptor(authorizedClientManager)).build();
+	}
+
+	@Bean
+	OAuth2RestClientHttpServiceGroupConfigurer oauth2RestClientConfigurer(
+			OAuth2AuthorizedClientManager auth2AuthorizedClientManager) {
+		return OAuth2RestClientHttpServiceGroupConfigurer.from(auth2AuthorizedClientManager);
 	}
 
 }
