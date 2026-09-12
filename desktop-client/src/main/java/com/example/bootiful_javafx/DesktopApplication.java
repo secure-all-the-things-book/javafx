8e5ce852bf532672a0dd8fc3eccb85f54904ff9f
Josh Long <joshua.long@broadcom.com>
2026-09-12 17:49:30 -0500

    automatic save @ 2026-09-12T22:49:30.341090Z

--- desktop-client/src/main/java/com/example/bootiful_javafx/DesktopApplication.java
+++ desktop-client/src/main/java/com/example/bootiful_javafx/DesktopApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilte
 import org.springframework.boot.security.autoconfigure.web.servlet.ServletWebSecurityAutoConfiguration;
 import org.springframework.boot.security.oauth2.client.autoconfigure.servlet.OAuth2ClientWebSecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
@@ -18,20 +19,39 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.web.client.support.OAuth2RestClientHttpServiceGroupConfigurer;
 import org.springframework.web.service.registry.ImportHttpServices;
 
-@ImportHttpServices(MessageClient.class)
-@SpringBootApplication(exclude = {ServletWebSecurityAutoConfiguration.class, SecurityFilterAutoConfiguration.class,
-        UserDetailsServiceAutoConfiguration.class, OAuth2ClientWebSecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {ServletWebSecurityAutoConfiguration.class,// 
+        SecurityFilterAutoConfiguration.class,//
+        UserDetailsServiceAutoConfiguration.class, //
+        OAuth2ClientWebSecurityAutoConfiguration.class//
+})
 public class DesktopApplication {
 
     public static void main(String[] args) {
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_GLOBAL);
-        var applicationContext = new SpringApplicationBuilder(DesktopApplication.class).headless(false).run(args);
+        var applicationContext = new SpringApplicationBuilder(DesktopApplication.class)//
+                .headless(false) // 
+                .run(args);
+        // <.>
         Platform.startup(() -> applicationContext.publishEvent(new StageReadyEvent(new Stage())));
     }
+}
+
+// TODO update javafx.adoc to reflect that these are now three classes in three files!! 
+// todo add bullets narrating the code 
+
+@ImportHttpServices(MessageClient.class)
+@Configuration
+class MessageClientConfiguration {
+}
+
+@Configuration
+class OAuth2Configuration {
+
 
     @Bean
-    OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository registrations,
-                                                          OAuth2AuthorizedClientService authorizedClients, SystemBrowserOAuth2AuthorizedClientProvider browser) {
+    OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository registrations,//
+                                                          OAuth2AuthorizedClientService authorizedClients,// 
+                                                          SystemBrowserOAuth2AuthorizedClientProvider browser) {//
         var manager = new AuthorizedClientServiceOAuth2AuthorizedClientManager(registrations, authorizedClients);
         manager.setAuthorizedClientProvider(
                 OAuth2AuthorizedClientProviderBuilder.builder().refreshToken().provider(browser).build());
@@ -39,8 +59,8 @@ public class DesktopApplication {
     }
 
     @Bean
-    OAuth2RestClientHttpServiceGroupConfigurer oauth2RestClientConfigurer(OAuth2AuthorizedClientManager manager) {
+    OAuth2RestClientHttpServiceGroupConfigurer oauth2RestClientConfigurer(//
+                                                                          OAuth2AuthorizedClientManager manager) {
         return OAuth2RestClientHttpServiceGroupConfigurer.from(manager);
     }
-
 }
